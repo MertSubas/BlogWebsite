6248336083d4d5dedae5d45af2e97746d7210b97
MertSubas <mertsubas@gmail.com>
2024-05-24 15:17:46 +0300

    Education Controller and View created

---
+++ GamzeBlogPsikolog/Areas/Admin/Controllers/EducationController.cs
@@ -0,0 +1,50 @@
+﻿using GamzeBlogPsikolog.Entity;
+using GamzeBlogPsikolog.Entity.Interfaces;
+using GamzeBlogPsikolog.EntityViewModels;
+using GamzeBlogPsikolog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamzeBlogPsikolog.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class EducationController : Controller
+    {
+        private readonly IEducationService educationService;
+
+        public EducationController(IEducationService educationService)
+        {
+            this.educationService = educationService;
+        }
+
+        public async Task<IActionResult> EducationIndex()
+        {
+            var education = await educationService.GetAll();
+            return View(education);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEducations([FromBody]EducationViewModel m)
+        {
+            AlertContent alert = new AlertContent();
+            if (ModelState.IsValid)
+            {
+                var educationAdd = await educationService.AddEducation(m);
+                alert.Message = "Kayıt Başarıyla Eklendi.";
+            }
+            return Json(alert);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetData(int id)
+        {
+            var geteducation = await educationService.GetByIdAsync(id);
+            return Json(geteducation);
+        }
+        [HttpPost]
+        public async Task<JsonResult> Delete(int id)
+        {
+            await educationService.DeleteEducation(id);
+            return Json("Ok");
+        }
+    }
+}
